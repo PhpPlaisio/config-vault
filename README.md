5eb284e681ee813a93ccae8bde0bece05fc6d1f1
Paul Water <p.r.water@setbased.nl>
2017-12-26 07:20:31 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# ABC-Framework: Config Vault
+
+<table>
+<thead>
+<tr>
+<th>Social</th>
+<th>Legal</th>
+<th>Release</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>
+<a href="https://gitter.im/SetBased/php-abc?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge"><img src="https://badges.gitter.im/SetBased/php-abc.svg" alt="Gitter"/></a>
+</td>
+<td>
+<a href="https://packagist.org/packages/setbased/abc-config-vault"><img src="https://poser.pugx.org/setbased/abc-config-vault/license" alt="License"/></a>
+</td>
+<td>
+<a href="https://packagist.org/packages/setbased/abc-config-vault"><img src="https://poser.pugx.org/setbased/abc-config-vault/v/stable" alt="Latest Stable Version"/></a>
+</td>
+</tr>
+</tbody>
+</table>
+
+
+#  License
+
+This project is licensed under the MIT license.
+
---
+++ src/ConfigVault.php
@@ -0,0 +1,51 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\ConfigVault;
+
+/**
+ * Interface for configuration vaults.
+ *
+ * A configuration vault is an object for storing safely sensitive configuration data such as API keys, private keys,
+ * and other configuration data using key-value pairs.
+ */
+interface ConfigVault
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the value stored under a key in a domain or all key-value pairs as an associative array in a domain.
+   *
+   * @param string      $domain The name of the domain.
+   * @param string|null $key    The key. If null all key-value pairs in the domain are returned.
+   *
+   * @return mixed
+   */
+  public function getValue($domain, $key = null);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Stores a value under a key in a domain or replaces all key-value pairs in a domain with key-value pairs given as an
+   * associative array.
+   *
+   * @param string      $domain The name of the domain.
+   * @param string|null $key    The key. If null the value must be an associative array.
+   * @param mixed       $value  The value.
+   *
+   * @return void
+   */
+  public function putValue($domain, $key, $value);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Removes a key from a domain or removes a whole domain.
+   *
+   * @param string      $domain The name of the domain.
+   * @param string|null $key    The key. If null a whole domain will be removed.
+   *
+   * @return void
+   */
+  public function unset($domain, $key = null);
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
